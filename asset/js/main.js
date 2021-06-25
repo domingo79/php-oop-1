e8d223b71e6b95ad1a0f38d355aa0fa7c1ef2f40
domingo79 <andria140779@yahoo.it>
2021-06-26 00:55:18 +0200

    non mi funziona lo script

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,10 @@
+// window.addEventListener('load', (event) => {
+//     console.log('page is fully loaded');
+// });
+
+
 var card = document.querySelector('.card');
+
 card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
 });
--- index.php
+++ index.php
@@ -19,35 +19,45 @@ $titolo = 'Film On-line';
 </head>
 
 <body>
+
     <div class="container">
 
         <?php foreach ($movies as $move) : ?>
-            <div class="card">
-                <div class="card_img">
-                    <img src="<?= $move->poster; ?>" alt="">
-                </div>
-                <div class="card_head">
-                    <h2>Titolo: <?= $move->title; ?></h2>
-                </div>
-                <div class="card_body">
-                    <span>Genere: <?= $move->genre; ?></span>
-                    <span>Durata: <?= $move->duration; ?>'' min</span>
-                    <span>Voto:
-                        <?php for ($i = 0; $i < round($move->rating / 2, 0); $i++) : ?>
-                            <i class="fas fa-star"></i>
-                        <?php endfor; ?>
-                    </span>
-                    <span>Prezzo: € <?= $move->price; ?></span>
-                    <button class="btn">trama...</button>
-                </div>
-                <div class="card_footer">
-                    <span>Anno: <?= $move->year; ?> </span>
-                    <span>Regista: <?= $move->direction; ?></span>
+            <div class="scene">
+                <div class="card">
+                    <div class="card_face face_front">
+                        <div class="card_img">
+                            <img src="<?= $move->poster; ?>" alt="">
+                        </div>
+                        <div class="card_head">
+                            <h2>Titolo: <?= $move->title; ?></h2>
+                        </div>
+                        <div class="card_body">
+                            <span>Genere: <?= $move->genre; ?></span>
+                            <span>Durata: <?= $move->duration; ?>'' min</span>
+                            <span>Voto:
+                                <?php for ($i = 0; $i < round($move->rating / 2, 0); $i++) : ?>
+                                    <i class="fas fa-star"></i>
+                                <?php endfor; ?>
+                            </span>
+                            <span>Prezzo: € <?= $move->price; ?></span>
+                            <button class="btn">trama...</button>
+                        </div>
+                        <div class="card_footer">
+                            <span>Anno: <?= $move->year; ?> </span>
+                            <span>Regista: <?= $move->direction; ?></span>
+                        </div>
+                    </div>
+                    <div class="card_face face_back">
+                        <p><?= $move->descript ?></p>
+                    </div>
+
                 </div>
             </div>
         <?php endforeach; ?>
     </div>
     <script src="./asset/js/main.js"></script>
+
 </body>
 
 </html>
