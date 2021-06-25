5c1e9871e1ba6bb4b84cd218c9ee3ce8eddabfe8
domingo79 <andria140779@yahoo.it>
2021-06-25 17:03:11 +0200

    creato un db film

---
+++ database-film/db-movies.php
@@ -0,0 +1,68 @@
+<?php
+$movies = [
+    new Movie(
+        'film1',
+        'Ricomincio da te.',
+        'https://cdn.chili.com/images/public/cms/66/8d/bc/06/668dbc06-9d24-459a-bf61-7d7b1d998843.jpg?width=495',
+        9.99,
+        'Romantico',
+        110,
+        0,
+        6.00,
+        2019,
+        'Drake Doremus',
+        'La trentenne Daphne è alle prese con un bilancio della sua vita, dopo la fine di una relazione tormentata. A una festa, incontra due uomini affascinanti e completamente opposti, nonché migliori amici: Frank, il "cattivo ragazzo", bello e inaffidabile, e Jack, equilibrato e sensibile. Daphne si lascia ammaliare da entrambi, intrecciando relazioni amorose totalmente distinte che le permetteranno di (ri)scoprire la vita e se stessa. Ma il destino ha per lei altri piani.'
+    ),
+    new Movie(
+        'film2',
+        'QT8: The first eight',
+        'https://cdn.chili.com/images/public/cms/85/6f/a5/2d/856fa52d-801d-4d02-bcdc-53be52123e88.jpg?width=495',
+        6.99,
+        'Biografia',
+        120,
+        0,
+        6,
+        2019,
+        'Tara Wood',
+        'Un documentario sui primi 21 anni di carriera del regista Quentin Tarantino, con interviste ai suoi frequenti collaboratori.'
+    ),
+    new Movie(
+        'film3',
+        'Il primo re',
+        'https://cdn.chili.com/images/public/cms/18/de/c1/9f/18dec19f-089d-49ae-b648-04bb1faa5f61.jpg?width=495',
+        7.99,
+        'Avventura',
+        127,
+        1,
+        7,
+        2019,
+        'Matteo Rovese',
+        'Romolo (Alessio Lapice) e Remo (Alessandro Borghi), due fratelli che vivono allevando il loro gregge. Travolti da una spettacolare piena del fiume Tevere, vengono catturati, assieme ad altri - dai crudeli guerrieri di Alba Longa. Grazie alla loro astuzia, riescono a fuggire assieme agli altri prigionieri, portando con loro una vestale e il Sacro Fuoco che custodisce.'
+    ),
+    new Movie(
+        'film4',
+        'Tutta un\'altra vita',
+        'https://cdn.chili.com/images/public/cms/39/4a/85/67/394a8567-e6a4-4c81-9a32-9e9a7597f7d6.jpg?width=495',
+        7.99,
+        'Commedia',
+        103,
+        0,
+        6.5,
+        2019,
+        'Alessandro Pondi',
+        'Cosa faresti se il destino ti offrisse di diventare un’altra persona per una settimana? Gianni è un tassista romano con una routine consolidata. Ha una moglie e due figli e sogna di vincere la lotteria per cambiare il suo quotidiano scialbo e sempre uguale, incapace com’è di modificare il flusso della sua vita. E quando una coppia benestante, in partenza per una settimana alle Maldive, dimentica nel suo taxi le chiavi della loro meravigliosa villa da mille e una notte, Gianni azzarda: si impossessa della villa e della vita del suo proprietario, finendo in una giostra di emozioni mai vissute prima che culminano nell’incontro inaspettato con Lola.'
+    ),
+    new Movie(
+        'film5',
+        'Godzilla VS Kong',
+        'https://cdn.chili.com/images/public/cms/75/98/73/ac/759873ac-fc51-4efa-a4d3-804ed71d2d05.jpg?width=495',
+        13.99,
+        'Fantascienza',
+        113,
+        0,
+        5.5,
+        2020,
+        'Adam Wingard',
+        'Due leggende si scontrano quando Godzilla e Kong, le più potenti forze della natura, si affrontano in una spettacolare battaglia epocale. Quando la Monarch intraprende una pericolosa missione in un territorio sconosciuto alla ricerca dell\'origine dei titani, una cospirazione di umani minaccia di spazzare via tutte le creature, sia buone sia cattive, dalla faccia della terra.'
+    )
+];
--- index.php
+++ index.php
@@ -1,34 +1,7 @@
 <?php
 require __DIR__ . '/classes/Movie.php';
+require __DIR__ . '/database-film/db-movies.php';
 
-$movies = [
-    new Movie(
-        'film1',
-        'Ricomincio da te.',
-        'https://cdn.chili.com/images/public/cms/66/8d/bc/06/668dbc06-9d24-459a-bf61-7d7b1d998843.jpg?width=495',
-        9.99,
-        'Romantico',
-        110,
-        0,
-        6.00,
-        2019,
-        'Drake Doremus',
-        'La trentenne Daphne è alle prese con un bilancio della sua vita, dopo la fine di una relazione tormentata. A una festa, incontra due uomini affascinanti e completamente opposti, nonché migliori amici: Frank, il "cattivo ragazzo", bello e inaffidabile, e Jack, equilibrato e sensibile. Daphne si lascia ammaliare da entrambi, intrecciando relazioni amorose totalmente distinte che le permetteranno di (ri)scoprire la vita e se stessa. Ma il destino ha per lei altri piani.'
-    ),
-    new Movie(
-        'film2',
-        'QT8: The first eight',
-        'https://cdn.chili.com/images/public/cms/85/6f/a5/2d/856fa52d-801d-4d02-bcdc-53be52123e88.jpg?width=495',
-        6.99,
-        'Biografia',
-        120,
-        0,
-        6,
-        2019,
-        'Tara Wood',
-        'Un documentario sui primi 21 anni di carriera del regista Quentin Tarantino, con interviste ai suoi frequenti collaboratori.'
-    )
-];
 var_dump($movies);
 ?>
 
